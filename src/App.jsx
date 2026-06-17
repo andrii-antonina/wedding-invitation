3621b18a9f6c0b9e3c09e0aa53f672a7c3613dd1
Daria <dsemenii@arcos-inc.com>
2026-06-17 18:51:05 +0300

    Add "Save The Date" feature with interactive canvas, styles, and dependencies (MUI, Emotion). Configure IDE and gitignore.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import Hero from './components/Hero.jsx'
+import SaveTheDate from './components/SaveTheDate.jsx'
 import Location from './components/Location.jsx'
 import Schedule from './components/Schedule.jsx'
 import Details from './components/Details.jsx'
 import DressCode from './components/DressCode.jsx'
 import MapLocation from './components/MapLocation.jsx'
 import RSVP from './components/RSVP.jsx'
+import Countdown from './components/Countdown.jsx'
 
 function App() {
   return (
     <>
       <Hero />
+      <SaveTheDate />
       <Location />
       <MapLocation />
       <Schedule />
       <Details />
       <DressCode />
       <RSVP />
+      <Countdown />
     </>
   )
 }
